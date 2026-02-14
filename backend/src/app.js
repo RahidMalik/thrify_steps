f685f7cafac6b09b68b0ca3b63b739a2b4bec2a3
Rahidmalik <malikrahid011@gmail.com>
2026-02-14 19:58:07 +0500

    CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -46,7 +46,9 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
 }));
 app.use(
   helmet({
